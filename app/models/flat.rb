21ac72eddf103f0f110946c65fa41837567c8d7f
Ezequiel <epuyosdacosta@gmail.com>
2022-11-16 18:59:37 +0100

    Routes Model Controller View of flats

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -3,7 +3,6 @@ class Flat < ApplicationRecord
   validates :title, presence: true
   validates :city, presence: true
   validates :description, presence: true
-  validates :title, presence: true
   validates :bedrooms, numericality: { only_integer: true }
   validates :number_of_guests, numericality: { only_integer: true }
   validates :price_per_night, numericality: { only_integer: true }
