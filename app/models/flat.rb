90fd4723d63933caecae066148200ca29657a9ca
Ezequiel <epuyosdacosta@gmail.com>
2022-11-21 13:32:22 +0100

    Improve card flat on map & address autocomplete in form

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -10,6 +10,7 @@ class Flat < ApplicationRecord
   has_one_attached :photo
   validates :photo, presence: true
   validates :rating, presence: true
+  validates :address, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
