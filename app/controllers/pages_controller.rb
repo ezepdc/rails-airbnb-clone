c936a4137db5a819b194ae1c61301315710850da
Ezequiel <epuyosdacosta@gmail.com>
2022-11-23 10:22:25 +0100

    Home with flat results without login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,19 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @flats = policy_scope(Flat)
+    if params[:query].present?
+      @flats = Flat.search_by_title_city_and_description(params[:query])
+    else
+      @flats = Flat.all
+    end
+    @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude,
+        info_window: render_to_string(partial: "/flats/info_window", locals: { flat: flat }),
+        image_url: helpers.asset_url("home.png")
+      }
+    end
   end
 end
